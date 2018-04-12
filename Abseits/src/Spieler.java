22185fd445c5f43ea07e43cc3ec12e94784efb77
Sthristhair <32815941+Sthristhair@users.noreply.github.com>
2018-04-12 19:51:28 +0200

    Grundgerüst

--- Abseits/src/Spieler.java
+++ Abseits/src/Spieler.java
@@ -1,4 +1,15 @@
 
 public class Spieler {
-
+	
+	//Attribute
+	private String name;
+	private int augensumme;
+	private boolean status;
+	
+	//Konstruktor 
+	public Spieler(String name, int augensumme) {
+		this.name = name;
+		this.augensumme = 0;
+		this.status = true;
+	}
 }

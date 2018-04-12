3c3394daaff92f9af32e309b45a26de61558effd
pauzii <paulstarke.ps@gmail.com>
2018-04-12 23:00:16 +0200

    Kappa1111!!!

--- Abseits/src/Main.java
+++ Abseits/src/Main.java
@@ -2,8 +2,10 @@
 public class Main {
 
 	public static void main(String[] args) {
+		
 		Wuerfel wuerfel1 = new Wuerfel(5);
 		System.out.println(wuerfel1.wuerfeln());
+		//Andre ist viel cooler als Pauzii KappaPride
 	}
 
 }

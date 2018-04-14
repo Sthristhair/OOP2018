a05cb72ee88eb21c350e8d0bf955c162d555fbd0
Paul Starke <paulstarke.ps@gmail.com>
2018-04-14 20:12:42 +0200

    g

--- StillePost/src/oop2/Gitter.java
+++ StillePost/src/oop2/Gitter.java
@@ -2,13 +2,20 @@ package oop2;
 
 import java.util.ArrayList;
 
+
+import java.util.Random;
+
+
 public class Gitter {
 	//Attribute
 	private int width;
 	private int height;
+
 	private Feld[][] gitter;
 	
-	public Gitter(int width, int height){
+	private ArrayList<Mensch> persons;
+	
+	public Gitter(int width, int height, int personCount){
 		this.width = width;
 		this.height = height;
 		gitter = new Feld[width][height];
@@ -18,7 +25,19 @@ public class Gitter {
 				gitter[i][j] = new Feld(i,j);
 			}
 		}
+		this.persons = new ArrayList<>();
+		
+		Random rand = new Random();
+		for (int i = 0; i < personCount; i++) {
+			int x = rand.nextInt(width);
+			int y = rand.nextInt(height);
+			this.persons.add(new Mensch(x,y));
+		}
+		
+		this.persons.add(new Mensch("Anton Angeber", 1, 0, 0));
+		this.persons.add(new Mensch("Berta Blümchen", 2, width, height));
 	}
+
 	
     public ArrayList<Feld> getNachbarn(Feld start){
         ArrayList<Feld> nachbarn = new ArrayList<Feld>();
@@ -46,4 +65,15 @@ public class Gitter {
         
         return x < width  && x >= 0 && y < height  && y >= 0;
     }
+
+
+	public int getWidth() {
+		return width;
+	}
+
+	public int getHeight() {
+		return height;
+	}
+	
+
 }

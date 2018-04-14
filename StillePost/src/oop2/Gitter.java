fed8e27738e4ce2300e09b8e7908d8c116daaf0b
Sthristhair <32815941+Sthristhair@users.noreply.github.com>
2018-04-14 20:07:01 +0200

    Grundgerüst

--- StillePost/src/oop2/Gitter.java
+++ StillePost/src/oop2/Gitter.java
@@ -1,12 +1,18 @@
 package oop2;
 
+import java.util.ArrayList;
+import java.util.Random;
+
 public class Gitter {
 	//Attribute
 	private int width;
 	private int height;
+
 	private Feld[][] gitter;
 	
-	public Gitter(int width, int height){
+	private ArrayList<Mensch> persons;
+	
+	public Gitter(int width, int height, int personCount){
 		this.width = width;
 		this.height = height;
 		gitter = new Feld[width][height];
@@ -15,5 +21,25 @@ public class Gitter {
 				gitter[i][j] = new Feld(i,j);
 			}
 		}
+		this.persons = new ArrayList<>();
+		
+		Random rand = new Random();
+		for (int i = 0; i < personCount; i++) {
+			int x = rand.nextInt(width);
+			int y = rand.nextInt(height);
+			this.persons.add(new Mensch(x,y));
+		}
+		
+		this.persons.add(new Mensch("Anton Angeber", 1, 0, 0));
+		this.persons.add(new Mensch("Berta Blümchen", 2, width, height));
 	}
+
+	public int getWidth() {
+		return width;
+	}
+
+	public int getHeight() {
+		return height;
+	}
+	
 }
--- StillePost/src/oop2/Mensch.java
+++ StillePost/src/oop2/Mensch.java
@@ -1,11 +1,26 @@
 package oop2;
 
+import java.util.Random;
+
 public class Mensch {
 	//Attribute
-	private int meinung;
+	private String name;
+	private int opinion;
+	private int x,y;
 	
 	//Meinungen: 0 = unentschlossen, 1 = heiraten, 2 = heiraten nicht
-	public Mensch() {
-		this.meinung = 0;
+	public Mensch(int x, int y) {
+		this.opinion = 0;
+		this.x = x;
+		this.y = y;
+						
+	}
+	
+	public Mensch(String name, int opinion, int x, int y) {
+		this.name = name;
+		this.opinion = opinion;
+		this.x = x;
+		this.y = y;
+		
 	}
 }

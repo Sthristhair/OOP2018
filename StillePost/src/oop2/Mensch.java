a3bdf3b500921d1d3c8c0c41dbff56150050c361
Sthristhair <32815941+Sthristhair@users.noreply.github.com>
2018-04-14 20:14:05 +0200

    1

--- StillePost/src/oop2/Mensch.java
+++ StillePost/src/oop2/Mensch.java
@@ -8,14 +8,20 @@ public class Mensch {
 	private int opinion;
 	private int x,y;
 	
-	//Meinungen: 0 = unentschlossen, 1 = heiraten, 2 = heiraten nicht
+	
 	public Mensch(int x, int y) {
 		this.opinion = 0;
 		this.x = x;
 		this.y = y;
 						
 	}
-	
+	/**
+	 * 
+	 * @param name
+	 * @param opinion Meinungen: 0 = unentschlossen, 1 = heiraten, 2 = heiraten nicht
+	 * @param x
+	 * @param y
+	 */
 	public Mensch(String name, int opinion, int x, int y) {
 		this.name = name;
 		this.opinion = opinion;

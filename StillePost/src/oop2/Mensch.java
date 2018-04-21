8370c3fa614e6244e87c4586e707589925fe22b5
Sthristhair <32815941+Sthristhair@users.noreply.github.com>
2018-04-21 22:09:00 +0200

    Doch auf Deutsch!!! :(

--- StillePost/src/oop2/Mensch.java
+++ StillePost/src/oop2/Mensch.java
@@ -5,12 +5,12 @@ import java.util.Random;
 public class Mensch {
 	//Attribute
 	private String name;
-	private int opinion;
+	private int meinung;
 	private int x,y;
 	
 	
 	public Mensch(int x, int y) {
-		this.opinion = 0;
+		this.meinung = 0;
 		this.x = x;
 		this.y = y;
 						
@@ -18,15 +18,14 @@ public class Mensch {
 	/**
 	 * 
 	 * @param name
-	 * @param opinion Meinungen: 0 = unentschlossen, 1 = heiraten, 2 = heiraten nicht
+	 * @param meinung Meinungen: 0 = unentschlossen, 1 = heiraten, 2 = heiraten nicht
 	 * @param x
 	 * @param y
 	 */
 	public Mensch(String name, int opinion, int x, int y) {
 		this.name = name;
-		this.opinion = opinion;
+		this.meinung = opinion;
 		this.x = x;
 		this.y = y;
-		
 	}
 }
